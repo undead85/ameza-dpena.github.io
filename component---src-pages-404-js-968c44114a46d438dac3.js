46334c94e43d6fce1bf1cbadd4e0a98f1a174ce2
Angel Meza <undead.85@gmail.com>
2019-10-09 11:18:36 -0300

    Updates

---
+++ component---src-pages-404-js-968c44114a46d438dac3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{211:function(e,n,a){"use strict";a.r(n);var t=a(0),l=a.n(t),r=a(209);n.default=function(){return l.a.createElement(r.a,{fullMenu:!0},l.a.createElement("article",{id:"main"},l.a.createElement("header",null,l.a.createElement("h2",null,"Not Found"),l.a.createElement("p",null,"Not a Valid URL"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-968c44114a46d438dac3.js.map
